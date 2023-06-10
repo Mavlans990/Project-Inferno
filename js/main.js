a2880d850fa756c3f2ff5f7dd5b79ccd3e9399eb
Maulana A <mavlans990@gmail.com>
2023-06-11 06:47:53 +0700

    Tambah file main.js

---
+++ js/main.js
@@ -0,0 +1,95 @@
+var header = 
+'<nav class="navbar navbar-expand-lg ps-25 pe-25">'+
+'    <div class="container-fluid">'+
+'        <a class="navbar-brand" href="#">'+
+'            <img src="Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />'+
+'            Inferno'+
+'        </a>'+
+'        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"'+
+'            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+'            <span class="navbar-toggler-icon"></span>'+
+'        </button>'+
+'        <div class="" id="navbarSupportedContent">'+
+'            <ul class="navbar-nav me-auto mb-2 mb-lg-0">'+
+'                <li class="nav-item dropdown-2">'+
+'                    <a class="nav-link active dropdown-toggle mt-5" style="line-height: 0px;" href="#" role="button"'+
+'                        data-bs-toggle="dropdown-2" aria-expanded="false">'+
+'                        Home'+
+'                    </a>'+
+'                    <ul class="dropdown-content" style="padding-left: 0px;list-style: none;margin-top: 10px;">'+
+'                        <li><a class="dropdown-item" href="index.html">Home-1</a></li>'+
+'                        <li><a class="dropdown-item" href="index-2.html">Home-2</a></li>'+
+'                    </ul>'+
+'                </li>'+
+'                <li class="nav-item dropdown-2">'+
+'                    <a class="nav-link dropdown-toggle mt-5" style="line-height: 0px;" href="#" role="button"'+
+'                        data-bs-toggle="dropdown-2" aria-expanded="false">'+
+'                        Pages'+
+'                    </a>'+
+'                    <ul class="dropdown-content" style="padding-left: 0px;list-style: none;margin-top: 10px;">'+
+'                        <li><a class="dropdown-item" href="about-us.html">About Us</a></li>'+
+'                        <li><a class="dropdown-item" href="projects.html">Our Projects</a></li>'+
+'                        <li><a class="dropdown-item" href="team.html">Our Team</a></li>'+
+'                    </ul>'+
+'                </li>'+
+'                <li class="nav-item">'+
+'                    <a class="nav-link" href="#">Services</a>'+
+'                </li>'+
+'                <li class="nav-item">'+
+'                    <a class="nav-link" href="#">Project</a>'+
+'                </li>'+
+'                <li class="nav-item">'+
+'                    <a class="nav-link" href="#">Blog</a>'+
+'                </li>'+
+'                <li class="nav-item">'+
+'                    <a class="nav-link" href="#">Contact</a>'+
+'                </li>'+
+'            </ul>'+
+'        </div>'+
+'    </div>'+
+'</nav>'
+
+
+var footer = 
+'<footer class="container-fluid mt-5" style="">'+
+'    <div class="row ps-25 pe-25 pt-5 pb-5">'+
+'        <div class="col-md-4">'+
+'            <div class="d-flex">'+
+'                <a class="navbar-brand" href="#">'+
+'                    <img src="Logo.png" alt="Logo" width="35" height="35" class="me-2" />'+
+'                </a>'+
+'                <h1>Inferno</h1>'+
+'            </div>'+
+'            <p class="p-medium">It is a long established fact that a reader will be distracted lookings.</p>'+
+'            <div class="d-flex align-items-center p-4 ps-0" style="">'+
+'                <a href="#"><img src="Icon/Facebook.png" class="pe-5" alt="" /></a>'+
+'                <a href="#"><img src="Icon/Twitter.png" class="pe-5" alt="" /></a>'+
+'                <a href="#"><img src="Icon/Linkedin.png" class="pe-5" alt="" /></a>'+
+'                <a href="#"><img src="Icon/Instagram.png" class="pe-5" alt="" /></a>'+
+'            </div>'+
+'        </div>'+
+'        <div class="col-md-1"></div>'+
+'        <div class="col-md-2">'+
+'            <h5 class="text-start mb-4">Pages</h5>'+
+'            <a href="about-us.html" style="text-decoration: none;"><p class="p-medium">About Us</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Our Projects</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Our Team</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Contact Us</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Services</p></a>'+
+'        </div>'+
+'        <div class="col-md-2">'+
+'            <h5 class="text-start mb-4">Services</h5>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Kitchan</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Living Area</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Bathroom</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Dinning Hall</p></a>'+
+'            <a href="#" style="text-decoration: none;"><p class="p-medium">Bedroom</p></a>'+
+'        </div>'+
+'        <div class="col-md-3">'+
+'            <h5 class="text-start mb-4">Contact</h5>'+
+'            <p class="p-medium">55 East Birchwood Ave. Brooklyn, New York 11201</p>'+
+'            <p class="p-medium">contact@interno.com</p>'+
+'            <p class="p-medium">(123) 456 - 7890</p>'+
+'        </div>'+
+'    </div>'+
+'</footer>'
